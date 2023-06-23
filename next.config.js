2fca0962cf32da3238a805ae7de12c719c5d5bb0
forexms78 <49295076+forexms78@users.noreply.github.com>
2023-06-23 22:02:09 +0900

    9 Songs fetching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["mlijsgrrixtkhnrtppbw.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
