c4dc0c5c8cb96d037b837ced7e22e11ca3f22d1a
forexms78 <49295076+forexms78@users.noreply.github.com>
2023-06-27 23:18:51 +0900

    11 Favorites functionality

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -2,13 +2,26 @@
 
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-interface SearchContentProps {
+interface LikedContentProps {
   songs: Song[];
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
   if (songs.length === 0) {
     return (
       <div
@@ -16,19 +29,17 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           flex 
           flex-col 
           gap-y-2 
-          w-full 
-          px-6 
+          w-full px-6 
           text-neutral-400
         "
       >
-        곡을 못 찾았어요 🥲
+        No liked songs.
       </div>
     );
   }
-
   return (
-    <div className="flex flex-col gap-y-2 w-full px-6">
-      {songs.map((song: Song) => (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song: any) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
@@ -40,4 +51,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   );
 };
 
-export default SearchContent;
+export default LikedContent;
