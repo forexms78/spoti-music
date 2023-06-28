18aea97d9aa37d9b661d381a9ae4dcd6b3507cd8
forexms78 <49295076+forexms78@users.noreply.github.com>
2023-06-28 23:54:11 +0900

    12 Player functionality (1)

---
+++ components/Slider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as RadixSlider from "@radix-ui/react-slider";
+interface SliderProps {
+  value?: number;
+  onChange?: (value: number) => void;
+}
+
+const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
+  const handleChange = (newValue: number[]) => {
+    onChange?.(newValue[0]);
+  };
+  return <RadixSlider.Root></RadixSlider.Root>;
+};
+
+export default Slider;
